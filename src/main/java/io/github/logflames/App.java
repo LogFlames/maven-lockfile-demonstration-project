72e246071adc86b083c95c4596d4e4fa3f53214b
Elias Lundell <elilun03@gmail.com>
2025-09-28 00:00:09 +0200

    :tada: init

---
+++ src/main/java/io/github/logflames/App.java
@@ -0,0 +1,12 @@
+package io.github.logflames;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
